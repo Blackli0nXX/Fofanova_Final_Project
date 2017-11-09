ff050576daa739ab19b06d86c3055ef63b8e981f
Blackli0nXX <starcraftguy@yahoo.com>
2017-11-08 19:47:21 -0600

    Initial commit

---
+++ src/Contact.java
@@ -0,0 +1,15 @@
+public class Contact {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String birthday;
+    private String notes;
+
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    
+
+}
