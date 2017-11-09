2193f2a11fe1764f25caa8a6560541812d4c727c
Blackli0nXX <starcraftguy@yahoo.com>
2017-11-09 14:41:42 -0600

    Add some setter methods to Contact class

--- src/Contact.java
+++ src/Contact.java
@@ -1,3 +1,6 @@
+import java.util.regex.*;
+import java.io.IOException;
+
 public class Contact {
 
     private String firstName;
@@ -9,7 +12,32 @@ public class Contact {
     private String notes;
 
     public void setFirstName(String firstName) { this.firstName = firstName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
-    
 
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public void setEmail(String email) throws InvalidEmailException{
+        if( Pattern.compile("\\S+@\\S+\\.\\S+").matcher( email ).matches() )
+            this.email = email;
+        else throw new InvalidEmailException();
+    }
+
+    public void setPhoneNumber(String phoneNumber) throws InvalidPhoneNumberException{
+        if( Pattern.compile("\\d{4,}").matcher( phoneNumber ).matches() )
+            this.phoneNumber = phoneNumber;
+        else throw new InvalidPhoneNumberException();
+    }
+
+    public void setAddress(String address) { this.address = address; }
+}
+
+class InvalidEmailException extends IOException{
+
+    public InvalidEmailException(){
+        super("Error: email must  contain one \'@\' and one \'.\'");
+    }
+}
+class InvalidPhoneNumberException extends IOException{
+    public InvalidPhoneNumberException(){
+        super("Error: number must have only digits and a minimum of four");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -9,14 +12,23 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        VBox root = new VBox();
+
+        Contact person = new Contact();
+
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
         launch(args);
     }
+
+    private Button newBtn = new Button("New");
+    private Button viewBtn = new Button("View");
+    private Button deleteBtn = new Button("Delete");
+    private Button editBtn = new Button("Edit");
+    private Button exitBtn = new Button("Exit");
+    private HBox selectAction = new HBox( newBtn, viewBtn, deleteBtn, editBtn, exitBtn );
 }
