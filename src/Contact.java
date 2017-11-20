c4e8ed69e2eb99dda8be27d0fda3674dc1bf817a
Trevor <ttouchet@protonmail.com>
2017-11-20 13:53:26 -0600

    Change fields to SimpleStringProperty datatype Add all getter methods Fix current setter methods

--- src/Contact.java
+++ src/Contact.java
@@ -1,32 +1,62 @@
+import javafx.beans.property.SimpleStringProperty;
 import java.util.regex.*;
 import java.io.IOException;
 
-public class Contact {
+public class Contact{
 
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String phoneNumber;
-    private String address;
-    private String birthday;
-    private String notes;
+    private SimpleStringProperty firstName;
+    private SimpleStringProperty lastName;
+    private SimpleStringProperty email;
+    private SimpleStringProperty phoneNumber;
+    private SimpleStringProperty address;
+    private SimpleStringProperty birthday;
+    private SimpleStringProperty notes;
 
-    public void setFirstName(String firstName) { this.firstName = firstName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
+    Contact( String firstName, String lastName, String email, String phoneNumber, String address, String birthday, String notes ){
+        this.firstName = new SimpleStringProperty( firstName );
+        this.lastName = new SimpleStringProperty( lastName );
+        this.email = new SimpleStringProperty( email );
+        this.phoneNumber = new SimpleStringProperty( phoneNumber );
+        this.address = new SimpleStringProperty( address );
+        this.birthday = new SimpleStringProperty( birthday );
+        this.notes = new SimpleStringProperty( notes );
+    }
+
+    public String getFirstName() { return this.firstName.get(); }
+    public SimpleStringProperty firstNameProperty() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName.set( firstName ); }
+
+    public String getLastName() { return this.lastName.get(); }
+    public SimpleStringProperty lastNameProperty() { return lastName; }
+    public void setLastName(String lastName) { this.lastName.set( lastName ); }
+
+    public String getEmail() { return email.get(); }
+    public SimpleStringProperty emailProperty() { return email; }
     public void setEmail(String email) throws InvalidEmailException{
         if( Pattern.compile("\\S+@\\S+\\.\\S+").matcher( email ).matches() )
-            this.email = email;
+            this.email.set( email );
         else throw new InvalidEmailException();
     }
+
+    public String getPhoneNumber() { return phoneNumber.get(); }
+    public SimpleStringProperty phoneNumberProperty() { return phoneNumber; }
     public void setPhoneNumber(String phoneNumber) throws InvalidPhoneNumberException{
         if( Pattern.compile("\\d{4,}").matcher( phoneNumber ).matches() )
-            this.phoneNumber = phoneNumber;
+            this.phoneNumber.set( phoneNumber );
         else throw new InvalidPhoneNumberException();
     }
 
-    public void setAddress(String address) { this.address = address; }
-    public void setBirthday(String birthday) { this.birthday = birthday; }
-    public void setNotes(String notes) { this.notes = notes; }
+    public String getAddress() { return address.get(); }
+    public SimpleStringProperty addressProperty() { return address; }
+    public void setAddress(String address) { this.address.set( address ); }
+
+    public String getBirthday() { return birthday.get(); }
+    public SimpleStringProperty birthdayProperty() { return birthday; }
+    public void setBirthday(String birthday) { this.birthday.set( birthday ); }
+
+    public String getNotes() { return notes.get(); }
+    public SimpleStringProperty notesProperty() { return notes; }
+    public void setNotes(String notes) { this.notes.set( notes ); }
 }
 
 class InvalidEmailException extends IOException{
